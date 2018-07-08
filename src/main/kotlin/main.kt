6e69a2c5530dc745c463b91ec6b86a5a20453b4c
Cornelius Jemison <cornelius.jemison@gmail.com>
2018-07-08 17:23:13 -0500

    cleaning up the logic a bit.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -15,12 +15,10 @@ fun main(args: Array<String>) {
     println(list.foldIndexed(mutableListOf<MutableList<String>>()) { index, acc, word ->
         if (!set.contains(word)) {
             val l = list.foldIndexed(mutableListOf<String>()) { index1, acc1, word1 ->
-                if (index != index1) {
-                    if (counts(word) == counts(word1)) {
-                        acc1.add(word)
-                        acc1.add(word1)
-                        set.add(word1)
-                    }
+                if (index != index1 && counts(word) == counts(word1)) {
+                    acc1.add(word)
+                    acc1.add(word1)
+                    set.add(word1)
                 }
                 acc1
             }
